f1cb842f5a86fe3f2c53cddbfb66e7024cf281ac
Mosisa Feyissa <mosisafaye@gmail.com>
2025-07-20 19:36:48 +0300

    Update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    output = "\n".join([f"{book.title} by {book.author}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
